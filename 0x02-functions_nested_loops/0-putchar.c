270f2a43623b37dbd2f5b29a4d057c1e972f3562
MizMelly <jazminelike@yahoo.com>
2023-06-20 04:09:38 -0700

    0 putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,20 @@
 #include "main.h"
 /**
- * main-print_putchar
+ * main - print _putchar
  * is written by Joy
- * Return : 0
+ * Return: 0
  */
-int main (void)
+
+int main(void)
 {
-	_putchar ('\n');
-	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
