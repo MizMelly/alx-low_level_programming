65ced50a7bfa169de0716fa4b2fb3cea80a526a3
MizMelly <jazminelike@yahoo.com>
2024-03-25 11:55:07 -0700

    2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -5,16 +5,18 @@
  * @n: The number to extract the bit from.
  * @index: The index of the bit to extract, starting from 0.
  *
- * Return: The value of the bit at the given index (0 or 1), or -1 if an error occurred.
+ * Return: The value of the bit at the given index (0 or 1),
+ *or -1 if an error occurred.
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-    if (index >= (sizeof(unsigned long int) * 8))
-	     return -1;
+	unsigned long int mts = 1UL << index;
 
-    unsigned long int mask = 1UL << index;
-    if (n & mask)
-	     return 1;
-    else
-        return 0;
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+
+	if (n & mts)
+		return (1);
+	else
+		return (0);
 }
